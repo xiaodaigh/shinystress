96614030df808db6bbae841a598728e13d21995f
susabi <zhuojia.dai@gmail.com>
2017-11-15 23:49:51 +1100

    generate some more data

--- src/0_gen_data.r
+++ src/0_gen_data.r
@@ -5,6 +5,7 @@ data_path <- "."
 
 
 state_prob <- c(7,5,3,2,1.2,0.5,0.3,0.4)
+risk_grade_prob <- 0.5*(1/2)^(0:19)
 
 gen_datatable_synthetic <- function(N=1e7) {
   data.table(
@@ -12,9 +13,9 @@ gen_datatable_synthetic <- function(N=1e7) {
     id3 = sprintf("id%010d",1:N), # id
     curr_bal =  rgamma(N,2,0.5)*500000, # numeric e.g. 23.5749
     int_rate = rep(0.05, N),
-    risk_grade = sample(1:20, replace = T, prob = c(0.0001*(1.55)^(1:20))),
+    risk_grade = sample(1:20, prob = risk_grade_prob),
     state = sample(c("NSW","VIC","QLD","WA","SA","TAS","NT","ACT"), replace = T, prob = state_prob/sum(state_prob))
-  )
+  )[,risk_grade_pd := c(0.0001*(1.55)^(1:20))[risk_grade]]
 }
 
 system.time(res <- gen_datatable_synthetic())
