0352ef5da9580e54050982fe0a7f3b67533c92b4
susabi <zhuojia.dai@gmail.com>
2017-11-15 23:45:35 +1100

    some code to generate the data

---
+++ src/0_gen_data.r
@@ -0,0 +1,24 @@
+library(data.table)
+library(fst)
+
+data_path <- "."
+
+
+state_prob <- c(7,5,3,2,1.2,0.5,0.3,0.4)
+
+gen_datatable_synthetic <- function(N=1e7) {
+  data.table(
+    month = rep(201711,N),
+    id3 = sprintf("id%010d",1:N), # id
+    curr_bal =  rgamma(N,2,0.5)*500000, # numeric e.g. 23.5749
+    int_rate = rep(0.05, N),
+    risk_grade = sample(1:20, replace = T, prob = c(0.0001*(1.55)^(1:20))),
+    state = sample(c("NSW","VIC","QLD","WA","SA","TAS","NT","ACT"), replace = T, prob = state_prob/sum(state_prob))
+  )
+}
+
+system.time(res <- gen_datatable_synthetic())
+pryr::object_size(res)
+
+write.fst(res,"simulation.fst", 100)
+system.time(read.fst("simulation.fst"))
